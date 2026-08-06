44f750991649def4b24c15b7f54d4be3c5b143b9
wangruoshi1991 <wangruoshi1991@users.noreply.github.com>
2026-08-06 13:11:13 +0800

    chore(mobile): remove unused asset registrations

--- MiaoxunRN/src/assets/icons/index.ts
+++ MiaoxunRN/src/assets/icons/index.ts
@@ -22,14 +22,6 @@ export const notificationIconAssets = {
   orderAssistant: asset(require('./notifications/order-assistant.png')),
 } as const;
 
-export const settingsIconAssets = {
-  digitalIdentity: asset(require('./settings/digital-identity.png')),
-  edit: asset(require('./settings/edit.png')),
-  files: asset(require('./settings/files.png')),
-  notifications: asset(require('./settings/notifications.png')),
-  permissions: asset(require('./settings/permissions.png')),
-} as const;
-
 export const composerIconAssets = {
   imageActive: asset(require('./composer/image-active.png')),
   imageEntry: asset(require('./composer/image-entry.png')),
@@ -69,19 +61,4 @@ export const stationPostIconAssets = {
   text: asset(require('./station/posts/text.png')),
   time: asset(require('./station/posts/time.png')),
   video: asset(require('./station/posts/video.png')),
-} as const;
-
-export const stationPartnerIconAssets = {
-  collectionOrganizer: asset(
-    require('./station/partners/collection-organizer.png'),
-  ),
-  eventCompanion: asset(require('./station/partners/event-companion.png')),
-  gigWork: asset(require('./station/partners/gig-work.png')),
-  marketplace: asset(require('./station/partners/marketplace.png')),
-  miaoxunAssistant: asset(require('./station/partners/miaoxun-assistant.png')),
-  personalBrand: asset(require('./station/partners/personal-brand.png')),
-  resale: asset(require('./station/partners/resale.png')),
-  travelInspiration: asset(
-    require('./station/partners/travel-inspiration.png'),
-  ),
 } as const;
